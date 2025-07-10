b19e5d1074d8c3944adbdffaf60cd8f07175a971
thatlukedev <luke@l2j.co.uk>
2025-07-10 10:06:41 +0100

    factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => ucfirst(fake()->word()),
+            'price' => fake()->randomFloat(2),
+            'image' => "https://picsum.photos/id/" . random_int(0, 1000) . "/200/300",
+            'description' => fake()->paragraph()
+        ];
+    }
+}
