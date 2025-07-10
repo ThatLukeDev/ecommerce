7a15d7edd5a7b853a55113878329617cbb98434d
thatlukedev <luke@l2j.co.uk>
2025-07-10 09:18:26 +0100

    responseiveness

--- resources/views/Components/productlisting.blade.php
+++ resources/views/Components/productlisting.blade.php
@@ -1,6 +1,6 @@
-<div class="m-5 p-5 bg-slate-200 rounded-xl">
-    <h1>{{ $product->name }}</h1>
-    <h2>{{ $product->price }}</h2>
-    <p>{{ $product->description }}</p>
-    <img src="{{ $product->image }}">
+<div class="m-5 p-5 min-h-60 bg-slate-200 rounded-xl">
+    <img class="md:float-right w-[100%] md:w-auto md:h-50 rounded-xl" src="{{ $product->image }}"><br>
+    <h1 class="text-5xl">{{ $product->name }}</h1>
+    <h2 class="text-3xl">{{ $product->price }}</h2>
+    <p class="text-xl">{{ $product->description }}</p>
 </div>
