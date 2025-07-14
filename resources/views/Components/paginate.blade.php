110af63f0c44b22971cdf2b7936bcf7e88172160
thatlukedev <luke@l2j.co.uk>
2025-07-14 09:25:44 +0100

    product listing styling

--- resources/views/Components/paginate.blade.php
+++ resources/views/Components/paginate.blade.php
@@ -1,5 +1,6 @@
+<?php if (request('page') == null) header("Location: ?page=1"); ?>
 <div class="flex justify-center m-10">
-    <a href="?page={{ request('page') > 1 ? request('page') - 1 : 1 }}" class="bg-gray-100 p-5 mr-5 rounded-full">&lt;</a>
+    <a href="?page={{ request('page') > 1 ? request('page') - 1 : $slot }}" class="bg-gray-100 p-5 mr-5 rounded-full">&lt;</a>
     <a class="p-5 rounded-full text-center">Showing page {{ request("page") ? request("page") : 1 }} out of {{ $slot }}</a>
-    <a href="?page={{ request('page') < $slot ? request('page') + 1 : $slot }}" class="bg-gray-100 p-5 ml-5 rounded-full">&gt;</a>
+    <a href="?page={{ request('page') < $slot ? request('page') + 1 : 1 }}" class="bg-gray-100 p-5 ml-5 rounded-full">&gt;</a>
 </div>
--- resources/views/Components/productlisting.blade.php
+++ resources/views/Components/productlisting.blade.php
@@ -1,5 +1,5 @@
 <div class="m-5 p-5 min-h-60 bg-slate-200 rounded-xl">
-    <img class="md:float-right w-[100%] md:w-auto md:h-50 rounded-xl" src="{{ $product->image }}"><br>
+    <img class="md:float-right md:ml-5 w-[100%] md:w-auto md:h-50 rounded-xl" src="{{ $product->image }}"><br>
     <a class="text-5xl">{{ $product->name }}</a>
     <a class="text-3xl">£{{ $product->price }}</a>
     <p class="text-xl">{{ $product->description }}</p>
