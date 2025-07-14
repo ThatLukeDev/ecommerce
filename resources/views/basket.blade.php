a516e5ec250fcdaa18ac19d412dd9fad916dc533
thatlukedev <luke@l2j.co.uk>
2025-07-14 14:42:45 +0100

    checkout button

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+$total = 0;
 
 ?>
 
@@ -16,8 +17,16 @@ use App\Models\Product;
         @foreach ($basket as $item => $amount)
             @php
                 $listing = Product::find($item);
+                $total += $listing->price * $amount;
             @endphp
             <x-basketitem :product=$listing>{{ $amount }}</x-basketitem>
         @endforeach
+
+        <p class="m-5 text-2xl mt-10">Total: £{{ number_format($total, 2) }}</p>
+        <span class="max-md:inline-flex justify-center w-full">
+        <form action="checkout" method="get" class="m-5">
+            <input type="submit" class="mr-2 p-5 px-20 bg-gray-300 rounded-full hover:bg-gray-400 hover:text-white" value="Checkout">
+        </form>
+        </span>
     </body>
 </html>
