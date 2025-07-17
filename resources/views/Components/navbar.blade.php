8e7b9a5f72a53ba2e09979786319d6961941ff3b
thatlukedev <luke@l2j.co.uk>
2025-07-17 14:48:31 +0100

    fixed stock clipping

--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-25"></div>
-<nav class="bg-gray-800 text-white p-5 top-0 fixed w-full inline-flex">
+<nav class="bg-gray-800 text-white p-5 top-0 fixed w-full inline-flex z-1">
     <span id="mainsearch" class="peer max-md:[.pressed]:w-full order-1000 group md:flex-auto inline-flex items-center bg-gray-700 p-2 mr-2 md:mr-10 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-5 m-1 md:mr-2" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
         <form action="/products" method="get" class="flex-auto">
--- resources/views/Components/productlisting.blade.php
+++ resources/views/Components/productlisting.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full h-full p-5 {{ $product->stock > 0 ? 'bg-slate-200' : 'bg-slate-400' }} rounded-xl">
         @if ($product->stock <= 0)
         <div class="relative w-full h-0">
-            <p class="text-white text-center bg-gray-600 rounded-full absolute px-5 py-4 m-5 size-20 right-0">No stock</p>
+            <p class="text-white text-center bg-gray-600 rounded-full absolute z-0 px-5 py-4 m-5 size-20 right-0">No stock</p>
         </div>
         @endif
         <img class="w-full rounded-xl aspect-4/3" src="{{ $product->image }}"><br>
