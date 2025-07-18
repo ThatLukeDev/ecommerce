18f61fa35b9dd157fe9ba8940e7c1894b1494391
thatlukedev <luke@l2j.co.uk>
2025-07-18 10:29:25 +0100

    fixed annoying admin panel

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -22,13 +22,13 @@ use App\Models\Home;
 
         <br><br>
 
-        @foreach ($items as $item)
-            <x-adminitem :product=$item></x-adminitem>
-        @endforeach
-
         <form class="m-10" action="{{ request()->path() }}/new" method="post">
             @csrf
             <div class="inline-flex justify-center w-full"><input type="submit" value="New" class="p-2 my-5 bg-indigo-400 text-white rounded-full w-100"></div>
         </form>
+
+        @foreach ($items as $item)
+            <x-adminitem :product=$item></x-adminitem>
+        @endforeach
     </body>
 </html>
