378a9340a01824b3401be5e413ead29bef6124d0
thatlukedev <luke@l2j.co.uk>
2025-07-18 11:35:39 +0100

    move add to cetner

--- resources/views/super/panel.blade.php
+++ resources/views/super/panel.blade.php
@@ -15,8 +15,8 @@
 
         <form class="mt-20 m-10" action="/{{ request()->path() }}/add" method="post">
             @csrf
-            <input type="text" name="add" placeholder="Email" class="p-2 my-2 w-100 bg-gray-200 rounded-t-xl rounded-b-lg outline-none border-b-[0.5vh] border-gray-400 focus:border-indigo-400 default:text-gray-600"><br>
-            <input type="submit" value="Add" class="p-2 my-5 bg-indigo-400 text-white rounded-full w-100">
+            <div class="inline-flex flex-auto justify-center w-full"><input type="text" name="add" placeholder="Email" class="p-2 my-2 w-100 bg-gray-200 rounded-t-xl rounded-b-lg outline-none border-b-[0.5vh] border-gray-400 focus:border-indigo-400 default:text-gray-600"><br></div>
+            <div class="inline-flex flex-auto justify-center w-full"><input type="submit" value="Add" class="p-2 my-5 bg-indigo-400 text-white rounded-full w-100"></div>
         </form>
     </body>
 </html>
